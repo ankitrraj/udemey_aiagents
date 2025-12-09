cc7668902942d52f737ad99b681e9c2fb5c82939
ankitrraj <ankitrajoria81@gmail.com>
2025-12-09 20:46:19 +0530

    list in python

---
+++ python_basics/list.py
@@ -0,0 +1,41 @@
+#append
+
+sher = ["foot","head","mouth"]
+sher.append("ankit")
+print(f"sher is {sher}")
+
+#insert extend append remove pop  reverse sort max min
+
+# extend 
+me = ["ankit","ankesh",'rohit']
+me2 = ["uttam","devansh","rohitk"]
+# me.extend(me2)
+# print(f"this is concat {me}")
+
+#insert
+me.insert(1,"buffalo")
+print(f"bufaalo added {me}")
+
+# remove
+me.remove("ankit")
+print(f"bufaalo added {me}")
+
+#pop
+me.pop()
+print(f"bufaalo added {me}")
+
+me.reverse()
+print(f"bufaalo added {me}")
+
+me.append("sherrrr")
+
+me.sort()
+print(f"bufaalo added {me}")
+
+create_a_new_list = [78,4,57,85,87897,545,87545,845787454,4841254]
+print(f"the min value is { max(create_a_new_list)}")
+print(f"the min value is { min(create_a_new_list)}")
+
+
+
+
