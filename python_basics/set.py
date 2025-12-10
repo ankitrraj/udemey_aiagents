6ec3b6a436119fe3d5643513c0e26f8064f9409e
ankitrraj <ankitrajoria81@gmail.com>
2025-12-10 15:07:53 +0530

    sherrrrrrrrrrrrr

---
+++ python_basics/set.py
@@ -0,0 +1,23 @@
+# set 
+
+animal = {"sher","bhes","tota","kabootr"}
+pepoles = {"auurat","adami","tabar","kabootr"}
+
+#  union 
+
+# alls = animal | pepoles
+# print(f"all are here {alls}")
+
+# intersection  (AUB)
+
+# alls_unique = animal & pepoles
+# print(f"all are here {alls_unique}")
+
+#  A-B  when it has {1,2,3,4} - {1,5,6,7,8,}  = {2,3,4}
+
+# minus = animal - pepoles
+# print(f"all are here {minus}")
+
+#  it has or not 
+print(f"the sher in people has {"sher" in animal}")
+
