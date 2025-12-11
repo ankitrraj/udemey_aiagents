781ee3935cbb5147c49087c6606962c7baaab9f7
ankitrraj <ankitrajoria81@gmail.com>
2025-12-11 17:48:53 +0530

    delivery cahrge system

---
+++ conditionals/delivery-charge-system.py
@@ -0,0 +1,6 @@
+order_amount = int(input("say what is your order amount:::"))
+
+if(order_amount<=300):
+    print(f"enjoy your delivery is free {order_amount}")
+else:
+    print(f"you pay 30 ruppes extra becuase your order value  {order_amount} is uper than 300 ")
