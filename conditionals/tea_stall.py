685200ff0db4029cdc07f604afe395f7250b139a
ankitrraj <ankitrajoria81@gmail.com>
2025-12-11 17:34:42 +0530

    tea-stall-problem

---
+++ conditionals/tea_stall.py
@@ -0,0 +1,12 @@
+user_input = input("hey what size of chai you order small-cup say small or medium-cup say medium or large-cup say large::").lower()
+small_cup_tea =10
+
+medium_cup_tea =15
+large_cup_tea =20
+
+if (user_input== "small"):
+    print("10 ruppes")
+elif (user_input== "medium"):
+    print("15 ruppes")
+else:
+    print("20 ruppes")
