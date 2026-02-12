95fa2039b59ddc9ee417f1d9a25c8c2aea1ccc88
ankitrraj <ankitrajoria81@gmail.com>
2026-02-12 22:19:06 +0530

    some chanegs

--- 09_oops/inheritance_compo.py
+++ 09_oops/inheritance_compo.py
@@ -5,3 +5,31 @@ class chai:
     def print_the(self):
         print(self.name)
         print("sher")
+class mas(chai):
+    chai_cls = chai
+
+    def __init__(self):
+        self.chai = self.chai_cls("reg")
+
+
+class Chaishop:
+    chai_cls =chai
+    def __init__(self):
+        self.chai =self.chai_cls("sher")
+
+    def serve(self):
+        print(f"serving {self.chai.type} chai in")
+        self.chai.prepare()
+
+class fancy(Chaishop):
+    chai_cls = mas
+
+
+shop =Chaishop()
+fancy = fancy()
+shop.serve()
+fancy.serve()
+
+
+
+
